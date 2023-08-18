b91ff9bcacd8da2a2eacb9d2ce1a9a6648c95e31
João Vitor <jvolima2004@gmail.com>
2023-08-18 13:42:47 -0300

    feat: course entity

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,3 +12,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://www.publicdomainpictures.net/pictures/270000/nahled/course-classroom.jpg', 'https://static.vecteezy.com/system/resources/previews/020/270/132/non_2x/study-icon-for-your-website-design-logo-app-ui-free-vector.jpg');
