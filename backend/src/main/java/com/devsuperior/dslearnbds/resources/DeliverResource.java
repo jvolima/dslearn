f6c6317eed583bff985b8b04f90ea66cd36ec2ab
João Vitor <jvolima2004@gmail.com>
2023-08-27 17:21:06 -0300

    feat: deliver revision

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/resources/DeliverResource.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dslearnbds.resources;
+
+import com.devsuperior.dslearnbds.dto.DeliverRevisionDTO;
+import com.devsuperior.dslearnbds.services.DeliverService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/deliveries")
+public class DeliverResource {
+
+    @Autowired
+    private DeliverService service;
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Void> saveRevision(@PathVariable Long id, @RequestBody DeliverRevisionDTO dto) {
+        service.saveRevision(id, dto);
+
+        return ResponseEntity.noContent().build();
+    }
+}
